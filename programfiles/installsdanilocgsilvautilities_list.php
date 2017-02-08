069cfdc958bfd5824bb0a364d506833ad2678588
Danilo Silva <danilocgsilva@gmail.com>
2017-02-08 19:46:37 -0200

    PHP file fetching is get robust. But I need to put authentication to expands the limit allowed to use the api

--- programfiles/installsdanilocgsilvautilities_list.php
+++ programfiles/installsdanilocgsilvautilities_list.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Automates the task of fetching the object from a curl web consult
+ * @param {string} url - The url of consult
+ * @param {curl} ch - The curl object
+ * @return {array}
+ */
 function extract_obj_curl($url, &$ch) {
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -9,15 +15,48 @@ function extract_obj_curl($url, &$ch) {
     return $obj_returned;
 }
 
+/**
+ * Check if the github project have a folder called "programfiles"
+ * @param {object} obs - The object representing the repository, fetched earlier
+ * @return {bool}
+ */
+function check_program_file_in_object_array($obs) {
+    $return_default = false;
+    foreach($obs as $node) {
+        if ($node->name == "programfiles" && $node->type == "dir") {
+            $return_default = true;
+            break;
+        }
+    }
+    return $return_default;
+}
+
+/**
+ * Fetches the data from url based on the repository name
+ * @param {string} repository_name - The string
+ * @return {bool}
+ */
 function check_if_programfiles_is_present($repository_name, &$ch) {
-    $filelist = extract_obj_curl("https://api.github.com/repos/danilocgsilva/" . $repository_name . "/contents/", $ch);
-    
+    $url_consult = "https://api.github.com/repos/danilocgsilva/" . $repository_name . "/contents/";
+    $filelist = extract_obj_curl($url_consult, $ch);
+    return check_program_file_in_object_array($filelist);
 }
 
 header("Content-Type: text/plain");
 
 $ch = curl_init();
+
+// Repository list
 $rep_list = extract_obj_curl('https://api.github.com/users/danilocgsilva/repos?per_page=10000', $ch);
+
 foreach ($rep_list as $rep) {
     echo $rep->name . "\n";
+
+    $programfiles_present = check_if_programfiles_is_present($rep->name, $ch);
+
+    if ($programfiles_present) {
+        echo "program file is here!\n";
+    } else {
+        echo "program file missing\n";
+    }
 }
